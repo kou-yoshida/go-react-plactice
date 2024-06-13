aa20dbca02c1801f18e092afe1167c3ab17528be
kou-yoshida <kouyoshida28@gmail.com>
2024-06-13 09:04:37 +0900

    🚧 チュートリアル途中まで

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,12 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 
-func Hello(name string) string{
+func Hello(name string) (string, error){
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message,nil
 }
